c044a40ca54c11251ddf0ef1fc08b66f62200d46
kaeiel <fab.brito94@gmail.com>
2019-02-17 08:43:10 -0300

    GUI modifications

--- stream_overlay.py
+++ stream_overlay.py
@@ -10,7 +10,9 @@ from requests_html import HTMLSession
 import psutil
 import re
 import os
+import string
 import time
+import logging
 
 
 class StreamOverlayAPP(tk.Tk):
@@ -18,53 +20,99 @@ class StreamOverlayAPP(tk.Tk):
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
 
-        self.animtext = ''
-        self.url = ''
-        self.obj = 0
+        logging.basicConfig(filename='errors.log', level=logging.ERROR,
+                            format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+
+        self.anim = 0
         self.followers = 0
         self.savedfollowers = 0
-        self.errors = []
-        self.resolution = ["1920", "1080"]
 
         self.ReadConfigFile()
 
+        self.generalfont = font.Font(
+            family=self.configs['general']['fontfamily'], size=self.configs['general']['fontsize'])
+        self.overlayfont = font.Font(
+            family=self.configs['overlay']['fontfamily'], size=self.configs['overlay']['fontsize'])
+
+        # Top frame
         topf = ttk.Frame()
-        bottomf = ttk.Frame()
         topf.pack(expand=True, fill=tk.BOTH, side=tk.TOP)
-        bottomf.pack(expand=True, fill=tk.BOTH, side=tk.TOP)
-
-        self.generalfont = font.Font(family='Helvetica', size=10)
-        self.overlayfont = font.Font(family='Helvetica', size=14)
-
-        self.button = ttk.Button(
-            topf, text='start', command=self.start, state='enabled')
-        self.button.pack(side=tk.RIGHT)
-        self.animation_label = ttk.Label(topf, text='', font=self.generalfont)
+        self.button_stop = ttk.Button(
+            topf, text='Stop', command=self.stop, state='disabled')
+        self.button_stop.pack(side=tk.RIGHT)
+        self.button_start = ttk.Button(
+            topf, text='Start', command=self.start, state='enabled')
+        self.button_start.pack(side=tk.RIGHT)
+        self.button_save = ttk.Button(
+            topf, text='Save', command=self.save, state='enabled')
+        self.button_save.pack(side=tk.RIGHT)
+        self.animation_label = ttk.Label(
+            topf, text='', font=self.generalfont)
         self.animation_label.pack(side=tk.LEFT)
-        self.errors_label = ttk.Label(topf, text='', font=self.generalfont)
-        self.errors_label.pack(side=tk.BOTTOM)
-        if len(self.errors) > 0:
-            self.button['state'] = 'disabled'
-            self.errors_label['text'] = '\n'.join(e for e in self.errors)
-            # print('\n'.join('>> ' + e for e in self.errors))
 
+        # Middle frame
+        midf = ttk.Frame()
+        midf.pack(expand=True, fill=tk.BOTH, side=tk.TOP)
+
+        self.url_label = ttk.Label(midf, text='URL: ', font=self.generalfont)
+        self.url_label.pack(side=tk.LEFT)
+        self.urlentry = ttk.Entry(midf, width=40, font=self.generalfont)
+        self.urlentry.insert(tk.INSERT, self.configs['save']['url'])
+        self.urlentry.pack(side=tk.LEFT)
+        self.objentry = ttk.Entry(midf, width=5, font=self.generalfont)
+        self.objentry.insert(tk.INSERT, self.configs['save']['objective'])
+        self.objentry.pack(side=tk.RIGHT)
+        self.obj_label = ttk.Label(midf, text='OBJ: ', font=self.generalfont)
+        self.obj_label.pack(side=tk.RIGHT)
+
+        # Bottom frame
+        botf = ttk.Frame()
+        botf.pack(expand=True, fill=tk.BOTH, side=tk.TOP)
         self.style = ttk.Style(self)
         self.style.theme_use('winnative')
-        self.style.layout('text.Horizontal.TProgressbar',
-                          [('Horizontal.Progressbar.trough',
-                            {'children': [('Horizontal.Progressbar.pbar',
-                                           {'side': 'left', 'sticky': 'ns'})],
-                             'sticky': 'nswe'}),
-                              ('Horizontal.Progressbar.label', {'sticky': ''})])
+        self.style.layout('text.Horizontal.TProgressbar', [('Horizontal.Progressbar.trough', {'children': [('Horizontal.Progressbar.pbar', {
+                          'side': 'left', 'sticky': 'ns'})], 'sticky': 'nswe'}), ('Horizontal.Progressbar.label', {'sticky': ''})])
         self.style.configure('text.Horizontal.TProgressbar', font=self.overlayfont,
-                             text='0 / 0', thickness=int(self.resolution[1]) // 40)
-        self.progress = ttk.Progressbar(bottomf, orient="horizontal", length=int(self.resolution[0]) // 4,
+                             text='0 / 0', thickness=self.configs['overlay']['progressbarthikness'])
+        self.progress = ttk.Progressbar(botf, orient="horizontal", length=self.configs['overlay']['progressbarlength'],
                                         mode="determinate", style='text.Horizontal.TProgressbar')
         self.progress.pack()
 
+    def save(self):
+        self.readInput()
+        data = ''
+        for name, dict_ in self.configs.items():
+            for name_, dict__ in dict_.items():
+                data += '{0}.{1}={2}\n'.format(name, name_, dict__)
+            data += '\n'
+
+        with open('stream_overlay.conf', 'w') as config_file:
+            config_file.write(data)
+        logging.info('Saved changes to the configurations file!')
+
+    def readInput(self):
+        self.configs['save']['url'] = re.findall(
+            r"https?://(?:[-\w.]|\/|(?:%[\da-fA-F]{2}))+", self.urlentry.get())[0]
+        self.configs['save']['objective'] = int(
+            re.findall(r"\d+", self.objentry.get())[0])
+
+    def stop(self):
+        self.urlentry['state'] = 'enabled'
+        self.objentry['state'] = 'enabled'
+        self.button_start['state'] = 'enabled'
+        self.button_stop['state'] = 'disabled'
+        self.button_save['state'] = 'enabled'
+        self.after_cancel(self.job)
+
     def start(self):
+        self.readInput()
         self.progress["value"] = 0
-        self.progress["maximum"] = self.obj
+        self.progress["maximum"] = self.configs['save']['objective']
+        self.urlentry['state'] = 'disabled'
+        self.objentry['state'] = 'disabled'
+        self.button_start['state'] = 'disabled'
+        self.button_stop['state'] = 'enabled'
+        self.button_save['state'] = 'disabled'
         self.getNimoTVFollowers()
 
     def ReadConfigFile(self):
@@ -72,54 +120,59 @@ class StreamOverlayAPP(tk.Tk):
             with open('stream_overlay.conf') as config_file:
                 data = config_file.read()
         except FileNotFoundError:
-            self.errors.append('File \"stream_overlay.conf\" not found!')
-        try:
-            line = re.findall(r"url ?= ?[\/\"\'.:\-\w%&?]+", data)[0]
-            self.url = re.findall(
-                r"https?://(?:[-\w.]|\/|(?:%[\da-fA-F]{2}))+", line)[0]
-        except IndexError:
-            self.errors.append('Missing nimo.tv url in configuration file!')
-        try:
-            line = re.findall(r"obj ?= ?\d+", data)[0]
-            self.obj = int(re.findall(r"\d+", line)[0])
-        except IndexError:
-            self.errors.append('Missing objective in configuration file!')
-        try:
-            line = re.findall(r"resolution ?= ?\d+x\d+", data)[0]
-            self.resolution = re.findall(r"\d+", line)
-        except IndexError:
-            self.errors.append('Missing resolution in configuration file!')
+            logging.error('Configurations file not found!')
+
+        self.configs = {'general': {}, 'overlay': {}, 'save': {}}
+        for configtype in ['general', 'overlay', 'save']:
+            crop_configs = re.findall(
+                configtype + r'.\w+ ?= ?[\"\w><\\\/ =.:]+', data)
+            for conf in crop_configs:
+                try:
+                    config_name = re.split(r' ?= ?', conf)[
+                        0][len(configtype) + 1:]
+                    config_value = re.split(r' ?= ?', conf)[1]
+                    if all(c in string.digits for c in config_value):
+                        config_value = int(config_value)
+                    self.configs[configtype][config_name] = config_value
+                except:
+                    logging.error(
+                        'Problem while loading the configurations file!')
 
     def getNimoTVFollowers(self):
-        if len(self.animtext) < 10:
-            self.animtext += ':'
-        else:
-            self.animtext = ''
-        self.animation_label['text'] = self.animtext
+        animtext = ["(~‾▿‾)~", " (~‾▿‾)~", "  (~‾▿‾)~", "   (~‾▿‾)~", "    (~‾▿‾)~", "    ~(‾▿‾~)", "   ~(‾▿‾~)", "  ~(‾▿‾~)", " ~(‾▿‾~)", "~(‾▿‾~)"]
+        self.animation_label['text'] = animtext[self.anim]
+        self.anim += 1
+        if self.anim >= len(animtext):
+            self.anim = 0
 
         session = HTMLSession()
-
-        r = session.get(self.url)
+        r = session.get(self.configs['save']['url'])
+        time.sleep(0.2)
         r.html.render()
+        time.sleep(0.2)
         page_source = str(r.html.html.encode('utf-8'))
+        session.close()
 
-        self.savedfollowers = self.followers
         try:
             crop = re.findall(
                 r"nimo-rm_followers[\"\w><\\\/ =.:]+followers:[\"\w><\\\/ =.:]+\d+", page_source)
             self.followers = int(re.findall(r"\d+", crop[0])[0])
-            self.errors_label['text'] = ''
+            if self.followers == 0:
+                self.followers = self.savedfollowers
+            else:
+                self.savedfollowers = self.followers
         except:
-            self.errors_label['text'] = 'Unable to retrieve the number of followers!'
+            logging.error('Unable to retrieve the number of followers!')
             self.followers = self.savedfollowers
 
-        session.close()
-        self.progress["value"] = self.followers
+        self.progress['value'] = self.followers
         self.style.configure('text.Horizontal.TProgressbar',
-                             text='{0} / {1}'.format(self.followers, self.obj))
-
-        if self.followers < self.obj:
-            self.after(10000, self.getNimoTVFollowers)
+                             text='{0} / {1}'.format(self.followers, self.configs['save']['objective']))
+        timeinterval = self.configs['overlay']['timeintervals']
+        if timeinterval == 0 or timeinterval > 3600:
+            timeinterval = 10
+        if self.followers < self.configs['save']['objective']:
+            self.job = self.after(timeinterval * 1000, self.getNimoTVFollowers)
 
 
 if __name__ == '__main__':
